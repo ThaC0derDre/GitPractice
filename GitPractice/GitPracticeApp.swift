5561ff45f62a9ca6454e58e4c61eb9d1a21e799a
AndresGutierrez <c0dingw.dre@gmail.com>
2022-05-09 08:31:41 -0700

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Andres Gutierrez on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
